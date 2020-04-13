ac25f4a31427590c72c6a1e382782de7b18d0ea2
LifelongLearner13 <sierragregg@gmail.com>
2020-04-13 13:19:00 -0500

    attribution component for data

---
+++ src/Attribution.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+
+const Attribution = () => {
+  return (
+    <Grid item>
+      <Typography variant={'h2'}>
+        Data provided by Marvel. © 2014 Marvel
+      </Typography>
+    </Grid>
+  );
+};
+
+export default Attribution;
